48c71c13d356d3e59e86f5c97eba0fc09559699b
Artsemi Stankevich <Artsemi.Stankevich@LeverX.local>
2020-12-20 19:39:36 +0300

    add postgreSQL and liquibase

--- src/main/java/com/leverx/actuator/springbootactuator/pojo/Product.java
+++ src/main/java/com/leverx/actuator/springbootactuator/pojo/Product.java
@@ -10,14 +10,19 @@ import javax.persistence.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Table(name = "products")
 public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false, unique = true)
     private String name;
 
     @Enumerated(EnumType.STRING)
+    @Column(name = "type", nullable = false)
     private ProductType productType;
+
+    @Column(nullable = false)
     private double price;
 }
